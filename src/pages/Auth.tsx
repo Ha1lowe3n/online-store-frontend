c93cea4c62e6bb369c175e720c818569dd1170b9
Igor Trifonov <Ha1lowe3n@yandex.ru>
2021-06-30 13:02:08 +0800

    end auth page

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,7 +1,69 @@
 import React from "react";
+import { NavLink, useLocation } from "react-router-dom";
+
+import { Button, Card, Container, Form, Row } from "react-bootstrap";
+import { PathRoutes } from "../routes";
 
 const Auth: React.FC = () => {
-    return <div>Auth</div>;
+    const location = useLocation(); // можем узнать путь
+    const isLogin = location.pathname === PathRoutes.LOGIN_ROUTE;
+
+    return (
+        <Container
+            className={"d-flex justify-content-center align-items-center"}
+            style={{ height: window.innerHeight - 54 }}
+        >
+            <Card style={{ width: 450, padding: 40 }}>
+                <h2>{isLogin ? "Авторизация" : "Регистрация"}</h2>
+                <Form className={"d-flex flex-column"}>
+                    <Form.Control
+                        className={"mt-4"}
+                        placeholder={"Введите ваш email"}
+                    />
+                    <Form.Control
+                        className={"mt-3"}
+                        placeholder={"Введите пароль"}
+                    />
+                    <Row
+                        className={
+                            "d-flex justify-content-between align-items-center m-0 mt-3 "
+                        }
+                    >
+                        {isLogin ? (
+                            <div
+                                className={
+                                    "d-flex flex-column align-items-start"
+                                }
+                            >
+                                <span style={{ textAlign: "left" }}>
+                                    Нет аккаунта?
+                                </span>
+                                <NavLink to={PathRoutes.REGISTRATION_ROUTE}>
+                                    Зарегистрируйся!
+                                </NavLink>
+                            </div>
+                        ) : (
+                            <div
+                                className={
+                                    "d-flex flex-column align-items-start"
+                                }
+                            >
+                                <span style={{ textAlign: "left" }}>
+                                    Есть аккаунт?
+                                </span>
+                                <NavLink to={PathRoutes.LOGIN_ROUTE}>
+                                    Войдите!
+                                </NavLink>
+                            </div>
+                        )}
+                        <Button variant={"outline-success"}>
+                            {isLogin ? "Зарегистрироваться" : "Войти"}
+                        </Button>
+                    </Row>
+                </Form>
+            </Card>
+        </Container>
+    );
 };
 
 export default Auth;
